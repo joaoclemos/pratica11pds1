84b4c1719d3a1a2e5e0dc7ff15e8a63a20f08241
joaoclemos <joaoclemos22@gmail.com>
2025-11-24 23:30:51 -0300

    Q4 pronta

--- exc4.c
+++ exc4.c
@@ -1,6 +1,6 @@
 double mediaVetor(int v[], int n) {
-    if (n == 1) {
-        return (double)v[0];
+    if (n <= 0) {
+        return 0.0;
     }
     return ((mediaVetor(v, n - 1) * (n - 1)) + v[n - 1]) / n;
 }
